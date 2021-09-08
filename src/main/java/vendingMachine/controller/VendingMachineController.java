d5eb2da63ab5354eb6c57d901f0c67fb1a80a9bb
koola97620 <koola97620@nate.com>
2021-09-08 10:35:16 +0900

    feat: 투입금액 입력 기능 구현

--- src/main/java/vendingMachine/controller/VendingMachineController.java
+++ src/main/java/vendingMachine/controller/VendingMachineController.java
@@ -8,8 +8,11 @@ public class VendingMachineController {
     public void operate() {
         InputView inputView = new InputView();
 
-        Money vendingMachineMoney = new Money(inputView.inputVendingMachineMoney());
+        Money vendingMachineMoney = new Money(inputView.settingVendingMachinePossessedMoney());
         Products products = new Products(inputView.inputProducts());
+
+        Money inputMoney = new Money(inputView.inputMoney());
+
 
 
     }
